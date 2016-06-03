4d7effa52db0306b51f9a01bfa3a74efe2219f41
Guillaume Occuly <occuly.guillaume@gmail.com>
2016-06-03 02:40:09 +0200

    add superman request

--- jsonFormat/ViewController.swift
+++ jsonFormat/ViewController.swift
@@ -13,6 +13,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://www.omdbapi.com/?s=superman&page=2")
+        
+        let request = NSURLSession.sharedSession().dataTaskWithURL(url!) { (data, response, error) in
+            
+            if let urlContent = data {
+                
+                do {
+                
+                    let jsonDictionnary = try NSJSONSerialization.JSONObjectWithData(urlContent, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                    
+                    let datas = jsonDictionnary["Search"] as! NSArray
+                    
+                    for info in datas {
+                        
+                        let infoDict = info as! NSDictionary
+                        
+                        print(infoDict["Title"]!)
+                    }
+                
+                } catch {
+                
+                    print("error json")
+                }
+                
+            }
+            
+        }
+        
+        request.resume()
     }
 
     override func didReceiveMemoryWarning() {
